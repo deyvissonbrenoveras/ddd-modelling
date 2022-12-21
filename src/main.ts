5ace6aac9570de4aa002ec52c0e54f65c5c2580b
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-12-20 22:54:09 -0300

    jest config

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,3 @@ let customer = new Customer("123", "Deyvisson Breno");
 const address = new Address("Rua dois", 2, "12345-678", "São Paulo");
 customer.Address = address;
 customer.activate();
-
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
-
-const order = new Order("1", customer._id, [item1, item2]);
