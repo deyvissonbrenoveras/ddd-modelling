c6edd861cff9b41de5fa6cd1498d6e238ca576d7
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-12-20 22:43:32 -0300

    order tests

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -21,17 +21,23 @@ describe("Order unit tests", () => {
   });
 
   it("should calculate total", () => {
-    const item = new OrderItem("i1", "Item 1", 100);
-    const item2 = new OrderItem("i2", "Item 2", 200);
+    const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
+    const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
     const order = new Order("o1", "c1", [item]);
 
     let total = order.total();
 
-    expect(total).toBe(100);
+    expect(total).toBe(200);
 
     const order2 = new Order("o2", "c1", [item, item2]);
     total = order2.total();
+    expect(total).toBe(600);
+  });
 
-    expect(total).toBe(300);
+  it("should throw error if the item qtd is greater than 0", () => {
+    expect(() => {
+      const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+      const order = new Order("o1", "c1", [item]);
+    }).toThrowError("Quantity must be grater than 0");
   });
 });
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -24,10 +24,13 @@ export default class Order {
     if (this._items.length === 0) {
       throw new Error("Items are required");
     }
+    if (this._items.some((item) => item.quantity <= 0)) {
+      throw new Error("Quantity must be grater than 0");
+    }
     return true;
   }
 
   total(): number {
-    return this._items.reduce((acc, item) => acc + item._price, 0);
+    return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
   }
 }
