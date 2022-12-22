f7b18eeee2dada6fc735e1a7b4784e0505a0bc11
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-12-22 00:15:11 -0300

    Order Service

---
+++ src/service/order.service.spec.ts
@@ -0,0 +1,17 @@
+import Order from "../entity/order";
+import OrderItem from "../entity/order_item";
+import OrderService from "./order.service";
+
+describe("Order service unit tests", () => {
+  it("should get total of all orders", () => {
+    const item1 = new OrderItem("i1", "Item 1", 100, "p1", 1);
+    const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
+
+    const order = new Order("o1", "c1", [item1]);
+    const order2 = new Order("o2", "c1", [item2]);
+
+    const total = OrderService.total([order, order2]);
+
+    expect(total).toBe(500);
+  });
+});
