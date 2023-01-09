985a8d39774e91bbed8616265b743c446158191d
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2023-01-09 08:52:35 -0300

    Product Domain Event

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,16 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
+import EventDispatcher from "./event-dispatcher";
+
+describe("Domain events tests", () => {
+  it("should register an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(
+      1
+    );
+  });
+});
