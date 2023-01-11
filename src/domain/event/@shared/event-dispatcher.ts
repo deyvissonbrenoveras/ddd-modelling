5e51f6455cd3c4a553b8cc3d839fda8c4f876fef
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2023-01-10 22:00:39 -0300

    Event Dispatcher

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -7,13 +7,29 @@ export default class EventDispatcher implements EventDispatcherInterface {
   get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
     return this.eventHandlers;
   }
-  notify(event: EventInterface): void {}
+  notify(event: EventInterface): void {
+    const eventName = event.constructor.name;
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach((eventHandler) => {
+        eventHandler.handle(event);
+      });
+    }
+  }
   register(eventName: string, eventHandler: EventHandlerInterface): void {
     if (!this.eventHandlers[eventName]) {
       this.eventHandlers[eventName] = [];
     }
     this.eventHandlers[eventName].push(eventHandler);
   }
-  unregister(eventName: string, eventHandler: EventHandlerInterface): void {}
-  unregisterAll(): void {}
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
+  }
+  unregisterAll(): void {
+    this.eventHandlers = {};
+  }
 }
