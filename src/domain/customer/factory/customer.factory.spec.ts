7128f69f0aea7a9cbcedcefbf57b392385a34e52
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2023-02-08 16:21:51 -0300

    Customer factory

---
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -0,0 +1,20 @@
+import Address from "../value-object/address";
+import CustomerFactory from "./customer.factory";
+
+describe("Customer factory unit test", () => {
+  it("should create a customer", () => {
+    let customer = CustomerFactory.create("John");
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.address).toBeUndefined();
+  });
+  it("should create a customer with an address", () => {
+    const address = new Address("Street", 1, "13330-250", "São Paulo");
+    let customer = CustomerFactory.createWithAddress("John", address);
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.address).toBe(address);
+  });
+});
